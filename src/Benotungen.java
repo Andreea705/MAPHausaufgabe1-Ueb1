4f97e74688b793e83b7b65c6e12e818a27d98c0b
Andreea705 <andreeacristina831@gmail.com>
2025-10-21 11:48:57 +0300

    looked over

--- src/Benotungen.java
+++ src/Benotungen.java
@@ -1,6 +1,6 @@
 public class Benotungen {
     public int[] durchgefallen(int[] noten) {
-        for(int i = 0; i < noten.length; ++i) {
+        for(int i = 0; i < noten.length; i++) {
             if (noten[i] > 100 || noten[i] < 0) {
                 throw new IllegalArgumentException("Falsche Werte fuer die Noten");
             }
@@ -8,7 +8,7 @@ public class Benotungen {
 
         int count = 0;
 
-        for(int j = 0; j < noten.length; ++j) {
+        for(int j = 0; j < noten.length; j++) {
             if (noten[j] < 40) {
                 count++;
             }
@@ -17,7 +17,7 @@ public class Benotungen {
         int[] durchfallnoten = new int[count];
         int index = 0;
 
-        for(int i = 0; i < noten.length; ++i) {
+        for(int i = 0; i < noten.length; i++) {
             if (noten[i] < 40) {
                 durchfallnoten[index++] = noten[i];
             }
@@ -37,7 +37,7 @@ public class Benotungen {
             sum += (float)noten[i];
         }
 
-        return (float)Math.round(sum / (float)noten.length * 100.0F) / 100.0F;
+        return Math.round(sum / (float)noten.length * 100.0) / 100.0F;
     }
 
     private int rundenote(int note) {
